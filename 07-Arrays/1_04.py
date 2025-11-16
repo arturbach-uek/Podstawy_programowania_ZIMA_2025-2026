92fb59ddc4d9131b83f866e3496669f73fbbf6d2
Artur Bach <244679@student.uek.krakow.pl>
2025-11-16 17:05:02 +0100

    Created exercise files and added some solutions

---
+++ 07-Arrays/1_04.py
@@ -0,0 +1,15 @@
+###
+# Prints some array elements
+#
+arr = [2, 3, 7, 5, 4]
+
+print(arr)
+print("Number of elements: ", len(arr))
+print("First value: ", arr[0])
+print("Second value: ", arr[1])
+print("Last value: ", arr[-1])
+print("Last but one value: ", arr[len(arr) - 2])
+print("Sum of 1st and last: ", arr[0] + arr[-1])
+print("Last value: ", arr[len(arr) // 2])
+for i in range(len(arr)):
+    print(arr[i], end=" ")
